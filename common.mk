df0a1aa2008b4de153e865d190d5d0c812638255
xLexip <x@lexip.dev>
2021-12-29 10:54:27 +0100

    msm8998-common: Include Google Camera
    * Include a GCam prebuilt and override the existing cam packages
    * Package: com.google.android.GoogleCameraEng
    * APK Source: https://t.me/ngcam
    
    Signed-off-by: xLexip <x@lexip.dev>

--- common.mk
+++ common.mk
@@ -331,6 +331,17 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Prebuilts
+PRODUCT_PACKAGES += \
+    GoogleCamera
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/system/etc/permissions/com.google.android.GoogleCameraEng.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCameraEng.xml
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/system/etc/permissions/com.android.camera2.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.android.camera2.xml
+
+
 # Privapp Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
